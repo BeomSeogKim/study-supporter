c4f39c0bb6bd74fa3040d741e9849b5de3d95f87
BeomSeogKim <kbs4520@daum.net>
2023-12-12 17:58:35 +0900

    Initial commit

---
+++ src/main/java/org/tommy/dev/studysupporter/StudySupporterApplication.java
@@ -0,0 +1,13 @@
+package org.tommy.dev.studysupporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySupporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySupporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tommy/dev/studysupporter/StudySupporterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tommy.dev.studysupporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySupporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
